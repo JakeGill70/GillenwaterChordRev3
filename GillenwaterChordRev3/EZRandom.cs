d79f0f00b2e27c2dec714b9cf2e791462c226109
Jake Gillenwater <JakeGill70@gmail.com>
2020-10-22 01:15:48 -0400

    Create EZRandom.cs

---
+++ GillenwaterChordRev3/EZRandom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GillenwaterChordRev3
+{
+    class EZRandom
+    {
+        static Random rand = new Random();
+
+        public static int Next()
+        {
+            return rand.Next();
+        }
+
+        public static int Next(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}
