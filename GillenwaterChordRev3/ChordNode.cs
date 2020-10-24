eb761d51ba3c22fa40e118075e245f7a518a7eb4
Jake Gillenwater <JakeGill70@gmail.com>
2020-10-23 22:47:53 -0400

    Moved hash function into EZHash class

--- GillenwaterChordRev3/ChordNode.cs
+++ GillenwaterChordRev3/ChordNode.cs
@@ -11,26 +11,10 @@ namespace GillenwaterChordRev3
 
         public ChordNode(string ip, int port)
         {
-            id = GetHashString(ip + port);
+            id = EZHash.GetHashString(ip + port);
             this.ipAddress = ip;
             this.port = port;
         }
-
-        public static string GetHashString(string inputString)
-        {
-            // Get the hash of the string
-            // using SHA-256
-            using HashAlgorithm hasher = SHA256.Create();
-            byte[] hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-
-            StringBuilder sb = new StringBuilder();
-            foreach (byte b in hashBytes)
-                sb.Append(b.ToString("X2"));
-
-            return sb.ToString();
-        }
-    }
-
     public class LocalNode : ChordNode
     {
         public LocalNode(string ip, int port) : base(ip, port)
---
+++ GillenwaterChordRev3/EZHash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GillenwaterChordRev3
+{
+    public class EZHash
+    {
+        public static string GetHashString(string inputString)
+        {
+            // Get the hash of the string
+            // using SHA-256
+            using HashAlgorithm hasher = SHA256.Create();
+            byte[] hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hashBytes)
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+    }
+}
